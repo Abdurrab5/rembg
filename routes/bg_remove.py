8687fd95729092c39416fdc80a4d28c427c709c5
ABDURRAB <abdurrab5555@gmail.com>
2026-07-02 11:26:01 +0500

    Refactor bg_remove to include session and timing
    Modified bg_remove function to accept a Request object and include session handling. Added performance timing for processing duration.

--- routes/bg_remove.py
+++ routes/bg_remove.py
@@ -1,15 +1,24 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Request, HTTPException
 from fastapi.responses import StreamingResponse
 from services.bg_service import remove_bg
 import io
+import time
 
 router = APIRouter()
 
+
 @router.post("/bg-remove")
-async def bg_remove(file: UploadFile = File(...)):
-    output_image = await remove_bg(file)
+async def bg_remove(request: Request, file: UploadFile = File(...)):
+    start = time.perf_counter()
+
+    session = request.app.state.session
+
+    output = await remove_bg(file, session)
+
+    print(f"Processing took {time.perf_counter() - start:.2f} seconds")
 
     return StreamingResponse(
-        io.BytesIO(output_image),
+        io.BytesIO(output),
         media_type="image/png"
     )
+ 
